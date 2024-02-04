a899b55b3f9d3df103a57768982851cb51f29d9f
Tibs Proctor <tibebuproctor@gmail.com>
2024-02-04 14:15:08 -0800

    first commit

---
+++ student.py
@@ -0,0 +1,8 @@
+people=  [
+{'name':'Tibs','school':'University of Washington','Age': 24,
+'name':'Mel','school':'University of Washington','Age':22,
+'name':'Tam','school':'Willamate University','Age':23}
+]
+
+for i in people:
+	print(people[i])
