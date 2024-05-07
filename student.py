3201cbf61587c240c438d1ab0b1cd5dfa14f696a
Tibs Proctor <155201332+Tibsproctor@users.noreply.github.com>
2024-05-07 11:27:00 -0700

    Delete student.py

--- student.py
+++
@@ -1,8 +0,0 @@
-people=  [
-{'name':'Tibs','school':'University of Washington','Age': 24,
-'name':'Mel','school':'University of Washington','Age':22,
-'name':'Tam','school':'Willamate University','Age':23}
-]
-
-for i in people:
-	print(people[i])
